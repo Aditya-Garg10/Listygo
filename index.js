d90560afae32e756b3e2af3e6bad0eda107c6077
Sarman-Keshwani <sarmkesh@gmail.com>
2025-04-19 22:48:08 +0530

    ee

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use(cors({
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "https://listygo-fe-two.vercel.app/", // Replace with your frontend URL
   credentials: true
 }));
 
